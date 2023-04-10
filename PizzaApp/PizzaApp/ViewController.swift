6bfd999d9b200dd3e21dfec85eadc6f3bfa52b7e
Oriol Sanz <oriolsanz.smx@gmail.com>
2023-04-10 11:10:41 +0200

    ADD: base project

---
+++ PizzaApp/PizzaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaApp
+//
+//  Created by Oriol Sanz Vericat on 10/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
